36addd20249165221b872cbe4b4899b1a6e4aabe
ke-zhang-rd <kezhang@bnl.gov>
2019-09-06 14:19:20 -0400

    MNT: Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='inherit',
+      version='0.0.1',
+      description='',
+      url='https://github.com/ke-zhang-rd/inherit.git,
+      author='Ke Zhang',
+      author_email='kz2249@columbia.edu',
+      license='MIT',
+      packages=['inherit'],
+      zip_safe=False)
